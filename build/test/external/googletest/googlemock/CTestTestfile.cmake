79d793b05e99a927a0d3aaf6b4c4399d8268d676
garikmirakyan06 <garikmirakyan06@gmail.com>
2025-04-19 09:39:42 -0700

    changes

--- build/test/external/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/garik/Desktop/PQueueClone/Priority-queue-implementation-by-Heap/test/external/googletest/googlemock
-# Build directory: /home/garik/Desktop/PQueueClone/Priority-queue-implementation-by-Heap/build/test/external/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/test/external/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/garik/Desktop/PQueueClone/Priority-queue-implementation-by-Heap/test/external/googletest/googletest
-# Build directory: /home/garik/Desktop/PQueueClone/Priority-queue-implementation-by-Heap/build/test/external/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
