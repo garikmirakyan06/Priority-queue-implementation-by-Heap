3173c3209f113459b4295b32c49b6ab2c3f0d67f
garikmirakyan06 <garikmirakyan06@gmail.com>
2025-04-23 23:33:31 +0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,13 +2,14 @@
 
 TOTAL_CORES=$(nproc)
 CORES_34=$(( (TOTAL_CORES * 3) / 4 ))
+ROOT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
 
-cd ./app
+cd "$ROOT_DIR" || exit
 
 rm -rf bin
 rm -rf build
 mkdir build 
-cd build
+cd build || exit
 cmake ..
 make -j "$CORES_34"
 
